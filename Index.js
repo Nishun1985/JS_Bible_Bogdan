447abc1f7eb100731198df3d7d6919589b8039ef
Nishun <nishun.p@gmail.com>
2019-03-06 22:56:21 +0530

    Initial commit

---
+++ Index.js
@@ -0,0 +1,22 @@
+for (let i = 0; i <4 ; i++) {
+
+}
+describe ('', function () {
+
+});
+
+var Person = (function()){
+    // constructor
+    function Person(){
+
+    }
+    return Person;
+})();
+
+var myClass = (function()){
+    // constructor
+    function myClass(){
+
+    }
+    return myClass;
+})();
